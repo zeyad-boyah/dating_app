ae6f04ed6bd1a6d60f02a098d2767855ad04a2b5
Zeyad-Elgohary <zeyadelgohary.ze@gmail.com>
2025-09-23 07:44:37 +0300

    transfere to laptop commit

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,16 +8,22 @@ import { Component, inject, OnInit, signal } from '@angular/core';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App implements OnInit{
+export class App implements OnInit {
   private http = inject(HttpClient);
   protected readonly title = 'zeyad';
-  
-  
-  ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/members').subscribe({
-      next: response => console.log(response),
-      error: error =>console.log(error),
-      complete: ()=> console.log("api request complete") 
-  })
+  protected members = signal<any>([]);
+
+
+  async ngOnInit(){
+    this.members.set( await this.getMembers())
+  }
+
+  async getMembers(){
+    try {
+      return lastValueFrom(this.http.get('https://localhost:5001/api/members'));
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
   }
 }
