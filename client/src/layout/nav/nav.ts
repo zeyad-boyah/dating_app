5e44f86665658b616bc21619347abb60793561fa
zeyad-boyah <zeyadelgohary.ze@gmail.com>
2025-12-30 16:19:18 +0200

    adds a toast service

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -2,6 +2,7 @@ import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
 import { Router, RouterLink, RouterLinkActive } from "@angular/router";
+import { ToastService } from '../../core/services/toast-service';
 
 @Component({
   selector: 'app-nav',
@@ -11,6 +12,7 @@ import { Router, RouterLink, RouterLinkActive } from "@angular/router";
 })
 export class Nav {
   protected accountService =inject(AccountService);
+  protected toastSerivce = inject(ToastService);
   private router = inject(Router);
   protected creds: any = {};
 
@@ -20,7 +22,7 @@ export class Nav {
         this.router.navigateByUrl('members/');
         this.creds ={};
       },
-      error: error => alert(error.message),
+      error: error => this.toastSerivce.error(error.message),
     })
   }
 
