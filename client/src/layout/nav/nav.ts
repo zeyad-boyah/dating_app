7cbb70c27b16c1a4a754a1de725b42572ea9c0e2
zeyad-boyah <zeyadelgohary.ze@gmail.com>
2025-12-30 12:05:02 +0200

    now redirects after login & logout

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,7 +1,7 @@
 import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
-import { RouterLink, RouterLinkActive } from "@angular/router";
+import { Router, RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-nav',
@@ -10,13 +10,14 @@ import { RouterLink, RouterLinkActive } from "@angular/router";
   styleUrl: './nav.css',
 })
 export class Nav {
-  protected accountService =inject(AccountService)
-  protected creds: any = {}
+  protected accountService =inject(AccountService);
+  private router = inject(Router);
+  protected creds: any = {};
 
   login () {
     this.accountService.login(this.creds).subscribe({
       next: result => {
-        console.log(result);
+        this.router.navigateByUrl('members/');
         this.creds ={};
       },
       error: error => alert(error.message),
@@ -25,5 +26,6 @@ export class Nav {
 
   logout (){
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
